c5cc93ad35050b24a0db80ce552b07b0b8bf007c
Wojciech Konior <konior.wojciech@gmail.com>
2025-03-26 19:18:27 +0100

    Create device-support.cpp

---
+++ c-tutorial/opencl/device-support.cpp
@@ -0,0 +1,24 @@
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+
+#include <CL/cl.hpp>
+
+int main(){
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
+
+  _ASSERT(platforms.size() > 0);
+
+  auto platform = platforms.front();
+  std::vector<cl::Device> devices;
+  platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+  __ASSERT(devices.size() > 0);
+
+  auto device = devices.front();
+  auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
+  auto version = device.getInfo<CL_DEVICE_VERSION>();
+
+  device = devices[1];
+  vendor = device.getInfo<CL_DEVICE_VENDOR>();
+  version = device.getInfo<CL_DEVICE_VERSION>();
+}
