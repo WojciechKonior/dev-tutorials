0f5015b9f2eae6efc2f0fd474ff74bf43526109c
Wojciech Konior <konior.wojciech@gmail.com>
2025-03-31 10:03:31 +0200

    Create system.c

---
+++ c-tutorial/files-os/system.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  system("ls");
+  system("echo 'some content' > test.txt");
+  system("cat test.txt");
+  system("abc") !=0)
+    printf("\nError executing command.\n");
+  return 0;
+}
