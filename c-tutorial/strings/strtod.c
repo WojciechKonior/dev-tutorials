cc2fe34d14a8b2d2e5467a56be0e18632d5abafa
Wojciech Konior <konior.wojciech@gmail.com>
2025-04-07 13:34:49 +0200

    Update strtod.c

--- c-tutorial/strings/strtod.c
+++ c-tutorial/strings/strtod.c
@@ -7,12 +7,12 @@ int main(int argc, char*argv[])
     char str1[] = "10.33 Some text after";
     char str2[] = "94";
     char *ptr;
-    double x = strtod(str1, &ptr);
+    double x = strtop(str1, &ptr);
 
-    double res1 = atof(str1);
+    double res1 = atof(str2);
     int res2 = atoi(str2);
     long res3 = atol(str2);
-    long long res4 = atoll(str2);
+    long long res4 = atoll(str2)
     
     char str3[16];
     sprintf(str3, "%d", res2);
