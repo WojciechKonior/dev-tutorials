0e5fe99b302e8e14a28f1e632203e5f20f6f1298
Wojciech Konior <konior.wojciech@gmail.com>
2026-09-14 11:05:25 +0200

    Create ep7-hello-world.cpp

---
+++ cpp-tutorial/cpp-programming-language-mike-shah/ep7-hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){ // main function is a main entrypoint
+    cout << "Hello World from cout!" << endl;
+    printf("Hello World from printf");
+    return 0;
+}
